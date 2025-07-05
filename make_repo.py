8bceaeb009ce6a8f5cfff47333a58db90e0a4b6e
cineroom <107762313+cineroom1@users.noreply.github.com>
2025-07-05 18:03:44 -0300

    Add files via upload

--- make_repo.py
+++ make_repo.py
@@ -1,24 +1,34 @@
-# make_repo.py
 import os
 import hashlib
 
-def generate_addons_file(path='repo'):
+REPO_DIR = 'repo'
+OUTPUT_XML = 'addons.xml'
+OUTPUT_MD5 = 'addons.xml.md5'
+
+def generate_addons_file():
     addons = []
-    for addon in os.listdir(path):
-        addon_path = os.path.join(path, addon)
-        if os.path.isdir(addon_path):
-            addon_xml_path = os.path.join(addon_path, 'addon.xml')
-            with open(addon_xml_path, 'r', encoding='utf-8') as f:
-                addons.append(f.read().strip())
+    for addon in sorted(os.listdir(REPO_DIR)):
+        addon_path = os.path.join(REPO_DIR, addon)
+        addon_xml = os.path.join(addon_path, 'addon.xml')
+        if os.path.isdir(addon_path) and os.path.isfile(addon_xml):
+            with open(addon_xml, 'r', encoding='utf-8') as f:
+                content = f.read().strip()
+                if content.startswith('<?xml'):
+                    content = content[content.find('?>')+2:].strip()
+                addons.append(content)
+    with open(OUTPUT_XML, 'w', encoding='utf-8') as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n<addons>\n')
+        for addon in addons:
+            f.write(addon + '\n')
+        f.write('</addons>\n')
 
-    addons_xml = u"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addons>\n" + "\n".join(addons) + "\n</addons>\n"
-    with open("addons.xml", "w", encoding="utf-8") as f:
-        f.write(addons_xml)
-
-    md5_hash = hashlib.md5(addons_xml.encode('utf-8')).hexdigest()
-    with open("addons.xml.md5", "w") as f:
+def generate_md5():
+    with open(OUTPUT_XML, 'rb') as f:
+        data = f.read()
+    md5_hash = hashlib.md5(data).hexdigest()
+    with open(OUTPUT_MD5, 'w') as f:
         f.write(md5_hash)
 
-    print("addons.xml e addons.xml.md5 gerados com sucesso.")
-
-generate_addons_file()
+if __name__ == '__main__':
+    generate_addons_file()
+    generate_md5()
