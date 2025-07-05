df8bdd718f72d7f16592ef68d9ad33ae26ba6249
cineroom <107762313+cineroom1@users.noreply.github.com>
2025-07-05 16:03:41 -0300

    Delete repo directory

--- repo/plugin.video.cineroom/resources/lib/clear_cache.py
+++
@@ -1,7 +0,0 @@
-import xbmcgui
-from resources.lib.video_cache import CACHE
-
-def clear_cache():
-    """Limpa todo o cache de vídeos"""
-    VIDEO_CACHE.clear()
-    xbmcgui.Dialog().notification("Cache", "Todos os caches foram limpos", xbmcgui.NOTIFICATION_INFO)
