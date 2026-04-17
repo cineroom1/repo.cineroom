8b981656b86fd9d8ab11bc56a1e3edfbca6848d6
cineroom <107762313+cineroom2@users.noreply.github.com>
2026-04-17 18:49:02 -0300

    Add files via upload

--- make_repo.py
+++ make_repo.py
@@ -7,26 +7,33 @@ OUTPUT_MD5 = 'addons.xml.md5'
 
 def generate_addons_file():
     addons = []
+
     for addon in sorted(os.listdir(REPO_DIR)):
+        if addon.startswith('.'):
+            continue
+
         addon_path = os.path.join(REPO_DIR, addon)
         addon_xml = os.path.join(addon_path, 'addon.xml')
+
         if os.path.isdir(addon_path) and os.path.isfile(addon_xml):
             with open(addon_xml, 'r', encoding='utf-8') as f:
                 content = f.read().strip()
                 if content.startswith('<?xml'):
-                    content = content[content.find('?>')+2:].strip()
+                    content = content[content.find('?>') + 2:].strip()
                 addons.append(content)
+
     with open(OUTPUT_XML, 'w', encoding='utf-8') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n<addons>\n')
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write('<addons>\n')
         for addon in addons:
-            f.write(addon + '\n')
-        f.write('</addons>\n')
+            f.write(addon.strip() + '\n')
+        f.write('</addons>')
 
 def generate_md5():
     with open(OUTPUT_XML, 'rb') as f:
         data = f.read()
     md5_hash = hashlib.md5(data).hexdigest()
-    with open(OUTPUT_MD5, 'w') as f:
+    with open(OUTPUT_MD5, 'w', encoding='utf-8') as f:
         f.write(md5_hash)
 
 if __name__ == '__main__':
